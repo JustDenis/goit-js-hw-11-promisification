800f9d735b1c0b7180748f00bc56891549f448b4
Denis <denisdenis2343@gmail.com>
2020-06-21 21:59:11 +0300

    ready hw

---
+++ js/task-03.js
@@ -0,0 +1,32 @@
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const makeTransaction = transaction => {
+  return new Promise((resolve, reject) => {
+    const delay = randomIntegerFromInterval(200, 500);
+
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+      if (canProcess) {
+        const result = { id: transaction.id, delay };
+        resolve(result);
+      } else {
+        reject(Error(transaction.id));
+      }
+    }, delay);
+  });
+};
+
+const logSuccess = result =>
+  console.log(`Transaction ${result.id} processed in ${result.delay}ms`);
+
+const logError = id => console.log(`Error processing transaction ${id}. Please try again later.`);
+
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
